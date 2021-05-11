97bc4b156e3e7993e4488d25c7a1d9a52db0d369
Parth Sharma <parthsharma151@gmail.com>
2021-05-11 15:51:54 +0530

    add: API GET requests setup

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -1,14 +1,106 @@
 import React, {
     Fragment,
+    useContext,
+    useState,
+    useEffect
 } from 'react'
 
 import {
+    Button,
+    Text
 } from '@chakra-ui/react'
+import AuthContext from '../../store/auth'
 
 const Dashboard = () => {
+
+    const authCtx = useContext(AuthContext);
+    const authToken = 'Token ' + authCtx.token;
+    
+    const [userDetails, setUserDetails] = useState(null);
+    const [paymentStatistics, setPaymentStatistics] = useState(null);
+    const [allPayments, setAllPayments] = useState(null);
+
+    useEffect(() => {
+        userDetailsApiRequest();
+        getStatisticsApiRequest();
+        getAllPaymentsApiRequest();
+    }, []);
+
+    const userDetailsApiRequest = () => {
+        fetch('https://act-grants-crm.herokuapp.com/rest-auth/user/',
+                {   
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': authToken
+                    }
+                }
+            ).then(response => 
+                response.json().then(data => ({
+                    data: data,
+                    status: response.status
+                })
+            ).then(res => {
+                if(res.data){
+                    setUserDetails(res.data);
+                } else {
+                    alert("ERROR RETRIEVING CONTENT.");
+                }
+            }))
+    }
+
+    const getStatisticsApiRequest = () => {
+        fetch('https://act-grants-crm.herokuapp.com/donation/get_statistics/',
+                {   
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': authToken
+                    }
+                }
+            ).then(response => 
+                response.json().then(data => ({
+                    data: data,
+                    status: response.status
+                })
+            ).then(res => {
+                if(res.data){
+                    setPaymentStatistics(res.data);
+                } else {
+                    alert("ERROR RETRIEVING CONTENT.");
+                }
+            }))
+    }
+
+    const getAllPaymentsApiRequest = () => {
+        fetch('https://act-grants-crm.herokuapp.com/donation/get_all_donations/',
+                {   
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': authToken
+                    }
+                }
+            ).then(response => 
+                response.json().then(data => ({
+                    data: data,
+                    status: response.status
+                })
+            ).then(res => {
+                if(res.data){
+                    setAllPayments(res.data);
+                } else {
+                    alert("ERROR RETRIEVING CONTENT.");
+                }
+            }))
+    }
+
     return(
         <Fragment>
             <h1>DASHBOARD PAGE AFTER LOGIN</h1>
+            <Text>{JSON.stringify(userDetails)}</Text>
+            <Text>{JSON.stringify(paymentStatistics)}</Text>
+            <Text>{JSON.stringify(allPayments)}</Text>
         </Fragment>
     )
 }

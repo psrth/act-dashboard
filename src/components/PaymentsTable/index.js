b1f1d556b4ae2e4704cb71c9f91c882e6a5de010
Parth Sharma <parthsharma151@gmail.com>
2021-05-28 23:59:35 +0530

    edit: repeated headers bug fix

--- src/components/PaymentsTable/index.js
+++ src/components/PaymentsTable/index.js
@@ -18,47 +18,51 @@ const PaymentsTable = (props) => {
       <Table variant="simple" size="md">
         <TableCaption>ACT Donations from the {props.userDetails.company.name}</TableCaption>
         <Thead>
-            <Tr>
-            <Th>NAME</Th>
-            <Th>EMAIL ADDRESS</Th>
-            <Th>PHONE NUMBER</Th>
-            <Th>COUNTRY</Th>
-            <Th isNumeric>AMOUNT</Th>
-            { props.payment.map((payment, key) => (
-              <Fragment>
-                { ((payment.meta)) ? 
-                  <Fragment>
-                    { Object.entries(payment.meta).map((meta, index) => (
-                      <Th>{meta[0]}</Th>
-                    ))}
-                  </Fragment> : 
-                  null 
-                }
-              </Fragment>
-            ))}
-            </Tr>
+          <Tr>
+          <Th>NAME</Th>
+          <Th>EMAIL ADDRESS</Th>
+          <Th>PHONE NUMBER</Th>
+          <Th>COUNTRY</Th>
+          <Th isNumeric>AMOUNT</Th>
+          
+          { props.payment[0] ? 
+            <Fragment>
+              { ((props.payment[0].meta)) ? 
+                <Fragment>
+                  { Object.entries(props.payment[0].meta).map((meta, index) => (
+                    <Th>{meta[0]}</Th>
+                  ))}
+                </Fragment> : 
+                null 
+              }
+            </Fragment> : 
+            null
+          }
+          </Tr>
         </Thead>
 
         <Tbody>
-            {props.payment.map((payment, key) => (
-              <Tr>
-                <Td fontWeight="bold" color="grey.700">{payment.donor_name}</Td>
-                <Td fontWeight="medium" color="grey.500">{payment.donor_email}</Td>
-                <Td fontWeight="medium" color="grey.500">{payment.donor_phone}</Td>
-                <Td fontWeight="medium" color="grey.500">{payment.donor_country}</Td>
-                { props.isDomestic ? 
-                  <Td fontWeight="bold" color="grey.700" isNumeric>INR {payment.amount}</Td> :
-                  <Td fontWeight="bold" color="grey.700" isNumeric>USD {payment.amount}</Td> }
-                { ((payment.meta)) ? 
-                  <Fragment>
-                    { Object.entries(payment.meta).map((meta, index) => (
-                      <Td>{meta[1]}</Td>
-                    ))}
-                  </Fragment> : 
-                  null 
-                }
-              </Tr>
-            ))}
+          {props.payment.map((payment, key) => (
+            <Tr>
+              <Td fontWeight="bold" color="grey.700">{payment.donor_name}</Td>
+              <Td fontWeight="medium" color="grey.500">{payment.donor_email}</Td>
+              <Td fontWeight="medium" color="grey.500">{payment.donor_phone}</Td>
+              <Td fontWeight="medium" color="grey.500">{payment.donor_country}</Td>
+              
+              { props.isDomestic ? 
+                <Td fontWeight="bold" color="grey.700" isNumeric>INR {payment.amount}</Td> :
+                <Td fontWeight="bold" color="grey.700" isNumeric>USD {payment.amount}</Td> }
+              
+              { ((payment.meta)) ? 
+                <Fragment>
+                  { Object.entries(payment.meta).map((meta, index) => (
+                    <Td>{meta[1]}</Td>
+                  ))}
+                </Fragment> : 
+                null 
+              }
+            </Tr>
+          ))}
         </Tbody>
       </Table>
     </Box>
